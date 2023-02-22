863433ef093298f39e8c84c883a84e117a1785f3
Vedran Badrov <1902badrovvedran@gmail.com>
2023-02-22 16:34:38 +0000

    Add baseline gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   root 'main#home'
 
-  get "/aboutus", to: "main#aboutus"
-  get "services", to: "main#services"
-  get "/receiversender", to: "main#receiversender"
-  get "/currierportal", to: "main#currierportal"
-  get "/costcalculator", to: "main#costcalculator"
+  get '/aboutus', to: 'main#aboutus'
+  get 'services', to: 'main#services'
+  get '/receiversender', to: 'main#receiversender'
+  get '/currierportal', to: 'main#currierportal'
+  get '/costcalculator', to: 'main#costcalculator'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test"should not save item without a name" do
+      item = Item.new
+      assert_not item.save, "Saved item without name"
+    end
+
+    test "should save item with a name" do
+      item = Item.new(name: "Test")
+      assert item.save, "Saved item with name"
+    end
 end
