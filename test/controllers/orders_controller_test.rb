da62d227ebfeda085ff23b254c946cb5e49c8339
unknown <1902badrovvedran@gmail.com>
2023-05-11 14:36:49 +0100

    Tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,9 +1,4 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  test 'should set the user_id to the current user id' do
-    sign_in users(:one)
-    post orders_url, params: { order: { receiver_email: 'jackthereceiver@test.com' } }
-    assert_equal assigns(:order).user_id, users(:one).id
-  end
 end
